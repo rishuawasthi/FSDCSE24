2f51b7eb8d7cf560bee3dd8d08d07a60072def77
rishuawasthi <awasthi.rishu10@gmail.com>
2026-08-25 09:37:13 +0530

    worked on dom tree and appendChild and adding image

--- index.js
+++ index.js
@@ -75,15 +75,57 @@
 // loginHandler("rishu_5851","1235",login)
 
 //synchronization
-console.log ("one")
-console.log ("two")
-console.log ("three")
+// console.log ("one")
+// console.log ("two")
+// console.log ("three")
 
 
 //asynchronous
-setTimeout(()=>{console.log("two")},1000)
-console.log("three")
+// setTimeout(()=>{console.log("two")},1000)
+// console.log("three")
 
 
 //DOM
+
+const container=document.getElementById('container');
+const button=document.getElementById('btn');
+
+const h1=document.createElement('h1');
+console.log(h1)
+const img=document.createElement('img');
+console.log(container);
+console.log(button);
+const loader=document.createElement('h2');
+h1.innerText='ABES Engineering College';
+function ping(){
+    //alert('server ping')
+
+    container.innerHTML=`<div style=border:solid><h2>ABES ENGINEERING College</h2> 
+    <h3>Rishu Awasthi</h3>
+    <h3>Skills=>javascript,css,html</h3>
+    </div>`
+ 
+    h1.style.backgroundColor='cyan';
+    h1.style.color='red';
+    container.appendChild(h1);
+    try{
+    img.src='https://www.w3schools.com/js/img_htmltree_800.svg';
+    img.setAttribute('height',200);
+    img.setAttribute('width',200);
+    container.appendChild(img);}
+    catch(e){
+        loader.innerHTML=`<h2 style=color:red>Error in loading image</h2`;
+    }
+    finally{
+        //loader.innerHTML='';
+        container.removeChild(loader);
+    }
+
+    button.style.display='none';
+}
+
+
+button.addEventListener('click',ping)
+
+
 
