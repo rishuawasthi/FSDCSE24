04b83c1b4045837556e28d4427d84ed3ecac226a
rishuawasthi <awasthi.rishu10@gmail.com>
2026-09-08 10:26:35 +0530

    started vitereact

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import ICard from './components/ICard'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div style={{color:'pink',background:'lightblue',border:"2px solid red",width:"600px",height:"600px"}}>
+      <h2 style={{margin:' 100px 0px'}}>ABES Engineering College</h2>
+      <div style={{textAlign:"left"}}>
+      <h2>Name:Rishu Awasthi</h2>
+      <h2>Roll No:24000320100913</h2>
+      <h2>Branch:CSE</h2>
+      <h2>Section:CSE24</h2>
+      <h2>Skills:JS,Cloud</h2>
+      </div>
+      </div>
+
+  )
+}
+
+export default App
